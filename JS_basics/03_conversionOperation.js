a4955679060b26621f72e303aa572a7f8684a4b0
aniketghlp <79999395+aniketghlp@users.noreply.github.com>
2023-12-15 04:21:20 +0000

    conversion

---
+++ JS_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = true
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let scoreNumber = Number(score)
+
+//console.log(typeof scoreNumber);
+
+//console.table([score, scoreNumber]);
+
+//console.log(typeof NaN);
+
+// If we conver a string "33" to number it is successful 
+// "33abc" --> NaN
+// null --> 0
+// undefined --> NaN
+// true --> 1 fasle --> 0
+
+let isLoggedin = "A"
+let isLoggedinbool = Boolean(isLoggedin)
+//console.log(isLoggedinbool); 
+
+// 1 -> true 0 > false
+// "" , null , undefined -> false 
+// "A" true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+
+console.log(stringnumber);
+console.log(typeof stringnumber)
+//
