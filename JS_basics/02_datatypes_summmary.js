8da7ebf01979b049d95adeba5d1bb513f53b6897
aniketghlp <79999395+aniketghlp@users.noreply.github.com>
2023-12-15 06:26:10 +0000

    datatypes misc

---
+++ JS_basics/02_datatypes_summmary.js
@@ -0,0 +1,11 @@
+// primitive ds vs non-primitive  --- how data is stored in memory and how it is accessed
+/* Primitive - Call by value not address (copied and used)
+String , Number , Boolean , null ,undefined, Symbol( makes a value uniique) , Bigint  
+*/
+/* Refrence type (non-primitive) -- Call by refrence  (address) 
+Array , Object , functions
+*/
+
+//JS  is dynamic typed language for example we donot need to define variable type 
+// const number = 11  we donot need to define const number = int(11) or float(11) hence it is a dynamic language.
+//
